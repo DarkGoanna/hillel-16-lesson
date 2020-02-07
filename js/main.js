1bd78b03e7ed0f0d788917aed6f1f015555531dc
Artur <minipovorot@gmail.com>
2020-02-07 15:16:27 +0200

    16 урок

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-athlets__list").owlCarousel({
+      loop: true,
+      nav: false,
+      items: 1,
+      startPosition: 1,
+    });
+  });
+
+  $(document).ready(function(){
+    $(".owl-players__list").owlCarousel({
+      loop: true,
+      nav: false,
+      items: 1,
+      startPosition: 2,
+    });
+  });
